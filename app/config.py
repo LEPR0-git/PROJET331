e68d6700f37afeb84542bccb9a43ba7af05dbaf0
christ-ph <dadjichrist5@gmail.com>
2025-10-20 23:41:16 +0100

    premier mise a jour

--- app/config.py
+++ app/config.py
@@ -4,10 +4,16 @@ from dotenv import load_dotenv
 load_dotenv()  # lit le fichier .env si présent
 
 class Config:
-    # Connexion DB
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "postgresql://christ:12345678@localhost:5432/freelance"
+    # Variables DB séparées
+    DB_USER = os.getenv("DB_USER", "christ")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "12345678")
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_PORT = os.getenv("DB_PORT", "5432")
+    DB_NAME = os.getenv("DB_NAME", "freelance")
+
+    # Génération dynamique de l'URL SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # évite les warnings SQLAlchemy
 
@@ -19,3 +25,4 @@ class Config:
 
     # JSON
     JSON_SORT_KEYS = False  # garde l’ordre des champs JSON
+
